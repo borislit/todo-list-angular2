a04efec24c517da30fa39af1b8666fa6fdf62de2
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-04-07 11:43:13 +0300

    input component + console

--- src/app/todo-input.ts
+++ src/app/todo-input.ts
@@ -3,9 +3,13 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'todo-input',
   template: `
-              <input />
-              <button>Add</button>`
+              <input #newItem />
+              <button (click)="addItem(newItem.value)">Add</button>`
 })
 export class TodoInput {
   constructor() { }
+
+  addItem(item) {
+    console.log(item);
+  }
 }
