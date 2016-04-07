a71f16c158dfb35a0d028f1e666c0613c32a137e
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-04-07 11:50:44 +0300

    input component with two ways native events

--- src/app/todo-input.ts
+++ src/app/todo-input.ts
@@ -3,13 +3,15 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'todo-input',
   template: `
-              <input #newItem />
+              <input #newItem [value]="currentItem" (change)="currentItem = newItem.value" />
               <button (click)="addItem(newItem.value)">Add</button>`
 })
 export class TodoInput {
+  currentItem = '';
   constructor() { }
 
   addItem(item) {
     console.log(item);
+    this.currentItem = '';
   }
 }
