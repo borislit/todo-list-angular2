686de83e9cfecded423adbde78e9e58657b147cc
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-04-07 13:10:42 +0300

    viewencapsulation

--- src/app/todo-list.ts
+++ src/app/todo-list.ts
@@ -1,12 +1,16 @@
-import {Component, Input} from 'angular2/core';
+import {Component, Input, ViewEncapsulation} from 'angular2/core';
 
 @Component({
   selector: 'todo-list',
   template: `
               <h2>Todos</h2>
               <ul>
-                <li [hidden]="item.completed" *ngFor="#item of todos">{{item.value}}</li>
-              </ul>`
+                <li [ngClass]="{'completed': item.completed}" *ngFor="#item of todos">{{item.value}}</li>
+              </ul>`,
+  styles: [`.completed {
+              color: green;
+  }`],
+  encapsulation: ViewEncapsulation.Emulated
 })
 
 export class TodoList {
