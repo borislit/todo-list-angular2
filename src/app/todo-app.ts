958ca367f7375eb688691db483a076f5be64a7bc
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-04-07 11:38:47 +0300

    input component - not working

--- src/app/todo-app.ts
+++ src/app/todo-app.ts
@@ -1,8 +1,12 @@
 import {Component} from 'angular2/core';
+import {TodoInput} from "./todo-input";
 
 @Component({
   selector: 'todo-app',
-  template: `{{message}}`
+  template: `
+            <todo-input></todo-input>
+            <div>{{message}}</div>`,
+  directives: [TodoInput]
 })
 
 export class TodoApp {
